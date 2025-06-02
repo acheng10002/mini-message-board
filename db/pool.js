e84e6b7d45e6763584565045cc8e8f9b6e660288
Amy Cheng <109977623+acheng10002@users.noreply.github.com>
2025-06-02 11:39:52 -0400

    Update file for project

--- db/pool.js
+++ db/pool.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
+/*
 module.exports = new Pool({
   host: "localhost",
   user: "amycheng",
@@ -9,18 +10,14 @@ module.exports = new Pool({
   password: "q",
   port: 5432,
 });
+*/
 
-/*
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // uses SSL for cloud databases (Koyeb) automatically, disables SSL locally
-  ssl: process.env.DATABASE_URL.includes("koyeb")
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
   /* enables SSL only in production, SSL is disabled in development 
     ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
-      : false, 
-}); 
-*/
+      : false, */
+});
