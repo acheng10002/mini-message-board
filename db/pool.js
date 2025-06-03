7cedc310a7e565c504f990a4ffab801e3f8905c2
Amy Cheng <109977623+acheng10002@users.noreply.github.com>
2025-06-03 11:38:54 -0400

    Update file for project

--- db/pool.js
+++ db/pool.js
@@ -1,27 +1,12 @@
 require("dotenv").config();
+const fs = require("fs");
 
 const { Pool } = require("pg");
-
-/*
-module.exports = new Pool({
-  connectionString: undefined,
-  host: "localhost",
-  user: "amycheng",
-  database: "mini_message_board",
-  password: "q",
-  port: 5432,
-  ssl: false,
-});
-*/
 
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: true,
+    ca: fs.readFileSync("./db/certs/ca.pem"),
   },
-  /* enables SSL only in production, SSL is disabled in development 
-    ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false */
 });
